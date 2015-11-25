2f4dbf0f5c3fd201d951a8d0218b11dfa30377b5
Annyv2 <Annyv2@users.noreply.github.com>
2015-11-25 19:23:42 -0430

    Create auth0-variables

---
+++ example/socketsio-auth0-sample/auth0-variables.js
@@ -0,0 +1,5 @@
+(function (exports){
+    exports.AUTH0_DOMAIN = 'Your_Auth0_Domain';
+    exports.AUTH0_CLIENT_ID = 'Your_Auth0_Client_Id';
+    exports.AUTH0_CLIENT_SECRET = 'Your_Auth0_Client_Secret';
+})(typeof exports === 'undefined' ? this['auth0Variables']={}: exports);
