ac5c58cd6289b5c9c2ff0e38b77e00b7730aed64
Annyv2 <Annyv2@users.noreply.github.com>
2015-12-01 15:33:06 -0430

    Update index.js

--- example/socketsio-auth0-sample/index.js
+++ example/socketsio-auth0-sample/index.js
@@ -1,20 +1,23 @@
-var app = require('express')();
+var express = require('express');
+var app=express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var socketioJwt = require('socketio-jwt');
-var auth0Variables = require('./auth0-variables');
+var dotenv = require('dotenv');
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
+dotenv.load();
 
-app.get('/auth0-variables.js', function(req, res){
-	res.sendFile(__dirname + '/auth0-variables.js');
-});
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
 
 io
 	.on('connection', socketioJwt.authorize({
-		secret: Buffer(auth0Variables.AUTH0_CLIENT_SECRET, 'base64'),
+		secret: Buffer(JSON.stringify(process.env.AUTH0_CLIENT_SECRET), 'base64'),
 		timeout: 15000 // 15 seconds to send the authentication message
 	}))
 	.on('authenticated', function(socket){
@@ -24,7 +27,17 @@ io
 			io.emit('chat message', msg);
 		});
 	});
+	
+app.use(express.static(__dirname + '/public'));
+	
+app.get('/', function (req, res) {
+  res.render('index',
+  { env: env }
+  
+  )
+})	
 
 http.listen(3001, function(){
 	console.log('listening on *:3001');
 });
+
