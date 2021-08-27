1ee6b9563d35dd0af579ff265f2e6898f5c8e730
Divlo <contact@divlo.fr>
2021-08-27 17:17:44 +0200

    fix: tests and build failure

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: './src',
-  setupFilesAfterEnv: ['<rootDir>/__test__/setup.ts'],
-  collectCoverage: true,
-  coverageDirectory: '../coverage/'
-}
